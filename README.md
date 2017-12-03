f416f102aba8f855f16695c0cd86fe8f2427b3c5
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-12-03 16:17:38 +0800

    ready for testing

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # `maybe::maybe<T>`
 
-Like `v8::Maybe<T>`, an optional type.
+Like `v8::Maybe<T>`, an optional type.  Check out Haskell's
+[`Data.Maybe`](https://hackage.haskell.org/package/base-4.10.0.0/docs/Data-Maybe.html)
+for more insights.  Header only.
+
+## Testing
+
+```bash
+$ make test
+```
 
 ## License
 
--- maybe.h
+++ maybe.h
@@ -4,8 +4,77 @@ namespace maybe {
 
 template <typename T>
 class maybe {
+public:
+	inline bool
+	is_just() const
+	{
+		return has_value_;
+	}
+
+	inline bool
+	is_nothing() const
+	{
+		return !has_value_;
+	}
+
+	inline T
+	from_just() const
+	{
+		if (!is_just()) throw std::runtime_error("from_just: nothing");
+		return value_;
+	}
+
+	inline T
+	from_maybe(const T& v) const
+	{
+		return has_value_ ? value_ : v;
+	}
+
+	inline bool
+	operator==(const maybe& other) const
+	{
+		return (is_just() == other.is_just())
+			&& (!is_just() || from_just() == other.from_just());
+	}
+
+	inline bool
+	operator!=(const maybe& other) const
+	{
+		return !operator==(other);
+	}
 private:
-	maybe();
+	maybe()
+		: has_value_(false)
+	{
+		/* nop */
+	}
+
+	explicit maybe(const T& v)
+		: has_value_(true)
+		, value_(v)
+	{
+		/* nop */
+	}
+
+	bool has_value_;
+	T value_;
+
+	template <typename U> friend maybe<U> nothing();
+	template <typename U> friend maybe<U> just();
 };
+
+template <typename T>
+maybe<T>
+nothing()
+{
+	return maybe<T>();
+}
+
+template <typename T>
+maybe<T>
+just(const T& v)
+{
+	return maybe<T>(v);
+}
 
 } /* namespace maybe */
